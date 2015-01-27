e35781ebecaed30b68f4e2545f110352d34c56cc
Tatyana Kasatkina <kasatkina_t_s@mail.ru>
2015-01-27 21:38:15 +0300

    Double constraints were added.

--- Phase_trajectory.m
+++ Phase_trajectory.m
@@ -2,14 +2,14 @@ clear all
 close all
 clc
 
-global constr
+
 global constr1 constr2
 % 1) y' < constr
 % 2) y' > constr
 % 3) constr1 < y' < constr2
 
-Y_0 = [0.1 0.2   -0.05];
-Y_end = [2.5 0.5 0.5];
+Y_0 =   [0.1  0.3  -0.2];
+Y_end = [2.5  0.5  -0.5];
 
 N = 1000;
 
@@ -49,14 +49,16 @@ Psi = c0 + c1*(y - Y_0(1)) + c2*(y - Y_0(1)).^2 + c3*(y - Y_0(1)).^3;
 plot(y,Psi);
 
 constr = 0;
-
-N_shift = 50;
-
-cond = false;
+constr1 = 0.2;
+constr2 = 0.7;
 
 d_min = -100;
 d_max = 100;
 hd = 0.01;
+
+N_shift = 30;
+
+cond = false;
 
 while not(cond)
     flag = 0;
@@ -65,7 +67,7 @@ while not(cond)
         % ------- 1) y' < constr ------------------------------------------
         % if ((flag == 0)&&(Psi(i) >= constr))
         % ------- 2) y' > constr ------------------------------------------
-        if ((flag == 0)&&(Psi(i) <= constr))
+        if ((flag == 0)&&(Psi(i) <= constr1))
         % -----------------------------------------------------------------
             y_left(1) = Y_0(1) + (i-1 - N_shift)*dy;  % отступили N_shift шагов назад от критической точки
             y_left(2) = Psi(i - N_shift);
@@ -75,7 +77,7 @@ while not(cond)
         % -------1)  y' < constr ------------------------------------------
         % if ((flag==1)&&(Psi(i) < constr))
         % -------2) y' > constr -------------------------------------------
-        if ((flag==1)&&(Psi(i) > constr))
+        if ((flag==1)&&(Psi(i) > constr1))
         % -----------------------------------------------------------------
             y_right(1) = Y_0(1) + (i-1 + N_shift)*dy;
             y_right(2) = Psi(i + N_shift);
@@ -85,7 +87,7 @@ while not(cond)
     end
     
     for d=d_min:hd:d_max
-        cond = IsCurveExist(y_left, y_right, dy, d);
+        cond = IsCurveExist_down_constr(y_left, y_right, dy, d);
         if (cond == true)
             break;
         end
@@ -97,43 +99,9 @@ while not(cond)
 end
 
 
-% -------------∆елание добитьс€ более плавного поведени€ кривой------------
-% -------------ƒл€ этого эмпирически введено ограничение y_left(1) <= 0.4;
-% -------------ограничение y' < 0.9----------------------------------------
-% N_shift = 1;
-% 
-% while (true)
-%     flag = 0;
-%     for i=1:(N+1)
-%         if ((flag == 0)&&(Psi(i) >= constr))
-%             y_left(1) = Y_0(1) + (i-1 - N_shift)*dy;  % отступили N_shift шагов назад от критической точки
-%             y_left(2) = Psi(i - N_shift);
-%             y_left(3) = (c1 + 2*c2*(y_left(1) - Y_0(1)) + 3*c3*(y_left(1) - Y_0(1))^2)*y_left(2);
-%             flag = 1;
-%         end
-%         if ((flag==1)&&(Psi(i) < constr))
-%             y_right(1) = Y_0(1) + (i-1 + N_shift)*dy;
-%             y_right(2) = Psi(i + N_shift);
-%             y_right(3) = (c1 + 2*c2*(y_right(1) - Y_0(1)) + 3*c3*(y_right(1) - Y_0(1))^2)*y_right(2);
-%             break;
-%         end
-%     end
-%     if (y_left(1) <= 0.4)
-%         break;
-%     end
-%     N_shift = N_shift + 1;
-% end
-% 
-% for d=d_min:hd:d_max
-%     cond = IsCurveExist(y_left, y_right, dy, d);
-%     if (cond == true)
-%         break;
-%     end
-% end
-
 if (cond)
     % Ќаходим интервал по d, на котором ограничение не нарушаетс€
-    d_lims = d_interval(y_left, y_right, dy, d);
+    d_lims = d_interval_down_constr(y_left, y_right, dy, d);
     % —троим корректирующий отрезок при максимальном d
     replace_part = curve_synthesis(y_left, y_right, dy, d_lims(1));
     x = y_left(1):dy:y_right(1);
@@ -142,6 +110,60 @@ else
     disp('јлгоритм завершил свою работу');
 end
 
+%% ”бираю выход за ограничение на втором отрезке
+N_shift = 30;
+
+cond = false;
+
+while not(cond)
+    flag = 0;
+    % »щем границы интервала, на котором нарушаетс€ ограничение
+    for i=1:(N+1)
+        % ------- 1) y' < constr ------------------------------------------
+        if ((flag == 0)&&(Psi(i) >= constr2))
+        % ------- 2) y' > constr ------------------------------------------
+        % if ((flag == 0)&&(Psi(i) <= constr1))
+        % -----------------------------------------------------------------
+            y_left(1) = Y_0(1) + (i-1 - N_shift)*dy;  % отступили N_shift шагов назад от критической точки
+            y_left(2) = Psi(i - N_shift);
+            y_left(3) = (c1 + 2*c2*(y_left(1) - Y_0(1)) + 3*c3*(y_left(1) - Y_0(1))^2)*y_left(2);
+            flag = 1;
+        end
+        % -------1)  y' < constr ------------------------------------------
+        if ((flag==1)&&(Psi(i) < constr2))
+        % -------2) y' > constr -------------------------------------------
+        % if ((flag==1)&&(Psi(i) > constr1))
+        % -----------------------------------------------------------------
+            y_right(1) = Y_0(1) + (i-1 + N_shift)*dy;
+            y_right(2) = Psi(i + N_shift);
+            y_right(3) = (c1 + 2*c2*(y_right(1) - Y_0(1)) + 3*c3*(y_right(1) - Y_0(1))^2)*y_right(2);
+            break;
+        end
+    end
+    
+    for d=d_min:hd:d_max
+        cond = IsCurveExist_up_constr(y_left, y_right, dy, d);
+        if (cond == true)
+            break;
+        end
+    end
+    
+    if not(cond)
+        N_shift = N_shift + 1;
+    end
+end
+
+
+if (cond)
+    % Ќаходим интервал по d, на котором ограничение не нарушаетс€
+    d_lims = d_interval_up_constr(y_left, y_right, dy, d);
+    % —троим корректирующий отрезок при максимальном d
+    replace_part = curve_synthesis(y_left, y_right, dy, d_lims(2));
+    x = y_left(1):dy:y_right(1);
+    plot(x,replace_part,'r');
+else
+    disp('јлгоритм завершил свою работу');
+end
 
 
 
